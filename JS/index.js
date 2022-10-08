cf7b6bffca773b18e10eb909bfe469cfaf5e31ec
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2022-10-08 13:10:43 +0200

    create form.js

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,7 @@
 // creat bookmark button function ->change bookmark item color
 const bookmarkButton = document.querySelector('[js-data="bookmark-button"]');
 
-bookmarkButton.addEventListener("click", (event) => {
+bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
 });
 
@@ -18,4 +18,29 @@ answerButton.addEventListener("click", () => {
     answerButton.textContent = "Show answer";
   }
   pAnswer.classList.toggle("card__answer--active");
+});
+
+//create new Card
+
+const cardList = document.createElement("li");
+const article = document.createElement("article");
+const headline = document.createElement("2");
+const cardButton = document.createElement("button");
+const paragraph = document.createElement("p");
+const uList = document.createElement("ul");
+const listItem = document.createElement("li");
+const bookmark = document.createElement("div");
+const createBookmarkButton = document.createElement("button");
+const svg = document.createElement("svg");
+const svgPath = document.createElement("path");
+
+const form = document.querySelector('[js-data="form"]');
+const formButton = document.querySelector('[js-data="form__button"]');
+
+form.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  console.log("hey");
+  const formData = new FormData(evt.target);
+  const data = Object.fromEntries(formData);
+  console.log("hey" + data);
 });
